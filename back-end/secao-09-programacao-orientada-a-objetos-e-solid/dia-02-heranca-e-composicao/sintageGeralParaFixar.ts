20565a21186076417d1b190354755a5b15646b62
joao <joao.dev94@gmail.com>
2023-04-28 21:09:49 -0300

    lecture 9.2 para fixar exercicios

--- back-end/secao-09-programacao-orientada-a-objetos-e-solid/dia-02-heranca-e-composicao/sintageGeralParaFixar.ts
+++ back-end/secao-09-programacao-orientada-a-objetos-e-solid/dia-02-heranca-e-composicao/sintageGeralParaFixar.ts
@@ -1,5 +1,23 @@
 class Superclass {
-  constructor(public isSuper: boolean) {
-    this.isSuper = true;
+  constructor(public isSuper: boolean = true) {}
+
+  protect sayHello():void {
+    console.log('Olá Mundo!');
   }
 }
+
+class Subclass extends Superclass {
+  public sayHello2() {
+    this.sayHello();
+  }
+};
+
+const myFunc = (supersuper: Subclass) => {
+  supersuper.sayHello();
+}
+
+// const superClass = new Superclass();
+const subClass = new Subclass();
+
+// myFunc(superClass);
+// myFunc(subClass);
